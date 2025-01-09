df31e45d59c00bc49ccd67490c04326d04979325
aileen <aileenpadrontorres511@gmail.com>
2025-01-09 21:05:51 +0000

    5.4 parte 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,25 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function about()
-    {
-        $title = 'Acerca de nosotros';
-        $subtitle = 'Información de la empresa';
-        $description = 'Somos una empresa dedicada a brindar soluciones innovadoras.';
-        $author = 'Aileen';
+  public function about()
+  {
+    $data = [
+      'title' => 'Acerca de nosotros',
+      'subtitle' => 'Información de la empresa',
+      'description' => 'Somos una empresa dedicada a brindar soluciones innovadoras.',
+      'author' => 'Aileen'
+    ];
+    
 
-        return view('home.about', compact('title', 'subtitle', 'description', 'author'));
-    }
+    return view('home.about', $data);
+  }
+
+  public function index()
+  {
+    $data = [
+      'title' => 'Página principal'
+    ];
+
+    return view('home.index', $data);
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    $title = 'Página principal';
-    return view('home.index', compact('title'));
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/about', [HomeController::class, 'about'])->name('about');
