96ed0579c1b21312503cf94e0ce6ddbb1c00614e
Aileen <aileenpadrontorres511@.com>
2025-01-14 11:42:02 +0000

    5.6 parte 1

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+@section('title', 'Página Administrador')
+@section('content')
+<div class="card">
+  <div class="card-header">
+    Página principal del panel de control
+  </div>
+  <div class="card-body">
+    Bienvenido/a al panel de control. Utiliza el menú lateral para navegar entre las diferentes opciones.
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\AdminHomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -10,3 +11,5 @@ Route::get('/about', [HomeController::class, 'about'])->name('about');
 
 Route::get('/products', [ProductController::class, 'index'])->name('product');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('product.show');
+
+Route::get('/admin', [AdminHomeController::class, 'index'])->name('admin.home');
